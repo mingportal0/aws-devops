a54b9274103aedc105dd735cad732a481e8111df
mingportal0 <53987185+mingportal0@users.noreply.github.com>
2022-11-18 16:59:50 +0900

    postgresql date

--- src/main/java/net/cj/edu/aws/MainController.java
+++ src/main/java/net/cj/edu/aws/MainController.java
@@ -1,5 +1,11 @@
 package net.cj.edu.aws;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +16,29 @@ public class MainController {
     public String home() {
         return "<h1>aws-devops-v2</h1>";
     }
+    
+    @GetMapping("/date")
+	public String date() throws ClassNotFoundException {
+		Class.forName("org.postgresql.Driver");
+		String connurl = "jdbc:postgresql://aws-dev-db.cinopjszzh41.ap-northeast-2.rds.amazonaws.com:5432/postgres";
+		String user = "postgres";
+		String password = "12345678";
+		String result = "NO_DATA";
+
+		try (Connection connection = DriverManager.getConnection(connurl, user, password);) {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery("SELECT now() AS date");
+
+			while (rs.next()) {
+				result = rs.getString("date");
+			}
+			rs.close();
+			stmt.close();
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
 }
